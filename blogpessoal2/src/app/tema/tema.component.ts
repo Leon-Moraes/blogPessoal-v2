a11c3f4469540be2bef0df5c255ee194ca27f80b
Leon-Moraes <leo_o_nardo1@outlook.com>
2021-01-28 15:14:43 -0300

    Componente tema e consumo

--- blogpessoal2/src/app/tema/tema.component.ts
+++ blogpessoal2/src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
+import { TemaService } from './../service/tema.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
 
 @Component({
   selector: 'app-tema',
@@ -9,9 +11,12 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
-
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit(){
@@ -20,6 +25,21 @@ export class TemaComponent implements OnInit {
 
       this.router.navigate(['/entrar'])
     }
+    this.findAllTemas();
   }
 
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar() {
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp;
+      alert('Tema cadastrado com sucesso');
+      this.tema = new Tema();
+      this.findAllTemas();
+    })
+  }
 }
